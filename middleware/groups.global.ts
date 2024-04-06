6d1f4571d1e92365d27ad577518f759c339328b3
ReXDeep <ua.rexdeep@gmail.com>
2024-04-06 17:19:26 +0300

    v2

--- middleware/groups.global.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware(to => {
-    if (process.server) return
-    const store = useFileStore();
-    $fetch('/api/groups').then((groups: string[]) => {
-        if(groups.includes('user')) navigateTo('/403');
-        store.SetRoles(groups);
-    })
-})
--- run.bat
+++
@@ -1,5 +0,0 @@
-SET NITRO_PORT=3092 
-SET NITRO_DB_USER=root
-SET NITRO_DB_PASS=
-SET NITRO_DB_DB=vrp
-node .output/server/index.mjs
